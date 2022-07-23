8ca7ce2480bd54c352fed4110aba1d9629aaa5eb
3m1n3nc3 <mygames.ng@gmail.com>
2022-07-24 00:51:37 +0100

    AppInfo now returns collections

--- app/Services/AppInfo.php
+++ app/Services/AppInfo.php
@@ -6,18 +6,18 @@ class AppInfo
 {
     public static function basic()
     {
-        return [
+        return collect([
             'name' => 'GreyMultiverse',
             'version' => env('APP_VERSION', config('app.api.version.code', '1.0.0')),
             'author' => 'Greysoft',
             'updated' => env('LAST_UPDATE', '2022-06-20 02:27:53'),
-        ];
+        ]);
     }
 
     public static function api()
     {
-        return [
+        return collect([
             'api' => self::basic(),
-        ];
+        ]);
     }
 }
