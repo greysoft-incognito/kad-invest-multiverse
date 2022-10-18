f8b7dab54034a519df5462ebbcf74b493eb1be7a
3m1n3nc3 <mygames.ng@gmail.com>
2022-10-18 13:25:11 +0100

    Add scanned scope: Fix batch title

--- app/Console/Commands/ExportFormData.php
+++ app/Console/Commands/ExportFormData.php
@@ -119,14 +119,14 @@ class ExportFormData extends Command
         }
 
         if ($queue === true) {
-            SendReport::dispatch($this->form, $this->batch);
+            SendReport::dispatch($this->form, $this->batch, $title);
         } else {
-            $this->form->data_emails->each(function ($email)  {
+            $this->form->data_emails->each(function ($email) use ($title) {
                 RateLimiter::attempt(
                     'send-report:'.$email.$this->batch,
                     5,
-                    function () use ($email) {
-                        Mail::to($email->toString())->send(new ReportGenerated($this->form, $this->batch));
+                    function () use ($email, $title) {
+                        Mail::to($email->toString())->send(new ReportGenerated($this->form, $this->batch, $title));
                     },
                 );
             });
